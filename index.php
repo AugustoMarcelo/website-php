c8067f19616730fa1dbc001667cfc37a9a1542d0
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-07-10 21:57:55 -0300

    Adicionando metodos a classe

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-    echo 'Testando..';
+    class User {
+        public function __construct() {
+            //echo 'Construtor chamado';
+        }
+
+        public function __destruct() {
+            //echo 'Destrutor chamado';
+        }
+
+        public function register() {
+            echo 'Usuário registrado';
+        }
+
+        public function login($username, $password) {
+            $this->auth_user($username, $password);
+        }
+
+        public function auth_user($username, $password) {
+            echo $username . ' está logado.';
+        }
+    }
+
+    $User = new User;
+    //$User->register();
+    $User->login('Marcelo', '12344');
 ?>
